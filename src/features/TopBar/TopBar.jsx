230520263bc6bc221bf81e9dd3b0b6db133c8a14
Farkas Bence <farkas.bence27@gmail.com>
2024-12-24 00:34:30 +0100

    add grid to top bar

--- src/features/TopBar/TopBar.jsx
+++ src/features/TopBar/TopBar.jsx
@@ -1,22 +1,48 @@
-import { Box, Container, TextField } from "@mui/material";
+import { Box, Button, InputAdornment, TextField } from "@mui/material";
+import Grid from "@mui/material/Grid2";
+import SearchIcon from "@mui/icons-material/Search";
 
 function TopBar() {
   return (
-    <Container
-      maxwidth="false"
-      sx={{
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between",
-        width: 1,
-      }}
-    >
-      <Box sx={{ background: "red" }}>1</Box>
-      <Box sx={{}}>
-        <TextField />
-      </Box>
-      <Box sx={{ background: "red" }}>3</Box>
-    </Container>
+    <Grid container sx={{ padding: "20px 0", background: '#4A5568' }}>
+      <Grid size={4} sx={{}}></Grid>
+      <Grid
+        size={4}
+        display={"flex"}
+        justifyContent={"center"}
+        sx={{ padding: "0 40px" }}
+      >
+        <TextField
+          label="Search"
+          fullWidth={true}
+          variant="outlined"
+          slotProps={{
+            input: {
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+            },
+          }}
+        />
+      </Grid>
+      <Grid
+        size={4}
+        display={"flex"}
+        justifyContent={"flex-end"}
+        gap={"20px"}
+        sx={{}}
+      >
+        <Button variant="contained" sx={{ minWidth: "120px" }}>
+          Log In
+        </Button>
+        <Button variant="outlined" sx={{ minWidth: "120px" }}>
+          {" "}
+          Sign Up
+        </Button>
+      </Grid>
+    </Grid>
   );
 }
 
