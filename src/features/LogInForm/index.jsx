7ef3eefd72d0711ae1d64fd4aa9e3de4f427ec22
Farkas Bence <farkas.bence27@gmail.com>
2024-12-27 11:37:32 +0100

    add elements to login form

--- src/features/LogInForm/index.jsx
+++ src/features/LogInForm/index.jsx
@@ -1,10 +1,65 @@
-import { Box, Container } from "@mui/material";
+import { Box, Button, Divider, TextField, Typography } from "@mui/material";
 
 function LoginForm() {
   return (
-    <Container sx={{ background: "#4A5568" }}>
-      <Box sx={{ background: "primary.main" }}>aslkngasdlkfasdf</Box>
-    </Container>
+    <Box
+      sx={{
+        background: "#4A5568",
+        padding: "40px",
+        width: "1000px",
+      }}
+    >
+      <Box
+        component="form"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography
+          variant="h3"
+          component="h3"
+          align="center"
+          color="primary.contrastText"
+          sx={{ textTransform: "uppercase" }}
+        >
+          Please Log In
+        </Typography>
+        <TextField fullWidth label="Email or Username" />
+        <TextField fullWidth label="Password" />
+        <Button variant="outlined">Log In</Button>
+      </Box>
+      <Divider sx={{ margin: "20px" }} color="#fff" />
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: "20px",
+        }}
+      >
+        <Typography component="p" variant="p">
+          Or continue with
+        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            width: "100%",
+          }}
+        >
+          <Button variant="outlined">Google</Button>
+          <Button variant="outlined">Google</Button>
+          <Button variant="outlined">Google</Button>
+        </Box>
+      </Box>
+      <Divider sx={{ margin: "20px" }} color="#fff" />
+      <Typography component="p" variant="p" color="#fff">
+        Click <a href="/">here</a> for registration
+      </Typography>
+    </Box>
   );
 }
 
